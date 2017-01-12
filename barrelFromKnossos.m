9311187e7ca3b46308777a2c2f480ce4646fa3f4
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-01-12 17:16:33 +0100

    Fix: My gosh...

--- barrelFromKnossos.m
+++ barrelFromKnossos.m
@@ -14,6 +14,7 @@ function barrelFromKnossos(wkParam, brlParam, box)
         ceil(box(:, 2) ./ fileClen) - 1];
     
     % progress
+    tic;
     curCount = 1;
     fileCount = prod(diff(fileIds, 1, 2) + 1);
     
