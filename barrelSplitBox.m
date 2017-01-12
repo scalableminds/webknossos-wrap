889a8c2050f2ef46f1b7d5ceff8383b29b053578
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-01-12 13:04:21 +0100

    Fix: Improve barrelSplitBox

--- barrelSplitBox.m
+++ barrelSplitBox.m
@@ -1,9 +1,7 @@
 function boxes = barrelSplitBox(box, blockSize, cubeSize)
-    % config
-    optLoadSize = 128;
+    minLoadSize = 128;
     
-    % prepare output
-    boxes = zeros(3, 2, 0);
+    boxes = nan(3, 2, 0);
     boxWidth = diff(box, 1, 2);
     
     % sanity checks
@@ -14,48 +12,70 @@ function boxes = barrelSplitBox(box, blockSize, cubeSize)
     if all(boxWidth >= blockSize) ... % at least block size
        && all(boxWidth <= cubeSize) ... % not more than file size
        && all(isPowerOfTwo(boxWidth)) ... % cube length is power of two
-       && ~any(diff(boxWidth)) ... % box is actually a cube
-       && ~any(rem(box(:, 1), boxWidth)) % offset is multiple of cube size
+       && all(diff(boxWidth) == 0) ... % box is actually a cube
+       && all(rem(box(:, 1), boxWidth) == 0) % offset is multiple of cube size
         % box in agreement with file format
-        boxes = box; return;
+        boxes = box;
+        return;
     end
     
-    % enlarge box to fix with optimal load size
-    box(:, 1) = optLoadSize .* floor(box(:, 1) ./ optLoadSize);
-    box(:, 2) = optLoadSize .* ceil(box(:, 2) ./ optLoadSize);
+    fileIds = [ ...
+        floor(box(:, 1) ./ cubeSize), ...
+        ceil(box(:, 2) ./ cubeSize) - 1];
     
-    % decompose it into three-dimensional cubes
-    for curSizeLog2 = fliplr(log2(optLoadSize):log2(cubeSize))
-        curSize = 2 ^ curSizeLog2;
-        
-        curIds = [ ...
-             ceil(box(:, 1) ./ curSize), ...
-            floor(box(:, 2) ./ curSize) - 1];
-        
-        curBoxes = nan([6, diff(curIds, 1, 2)' + 1]);
-        [curBoxes(1, :, :, :), curBoxes(2, :, :, :), curBoxes(3, :, :, :)] = ...
-            ndgrid( ...
-                curSize .* (curIds(1, 1):curIds(1, 2)), ...
-                curSize .* (curIds(2, 1):curIds(2, 2)), ...
-                curSize .* (curIds(3, 1):curIds(3, 2)));
-        curBoxes = reshape(curBoxes, 3, 2, []);
-        
-        curBoxesDone = arrayfun(@(idx) ...
-            inBoxes(boxes, curBoxes(:, 1, idx)), 1:size(curBoxes, 3));
-        
-        % filter and complete bounding boxes
-        curBoxes = curBoxes(:, :, not(curBoxesDone));
-        curBoxes(:, 2, :) = curBoxes(:, 1, :) + curSize;
-        
-        % complete list of boxes
-        boxes = cat(3, boxes, curBoxes);
+    for fileX = fileIds(1, 1):fileIds(1, 2)
+        for fileY = fileIds(2, 1):fileIds(2, 2)
+            for fileZ = fileIds(3, 1):fileIds(3, 2)
+                curBoxes = barrelSplitFileBox( ...
+                    box, [fileX, fileY, fileZ], minLoadSize, cubeSize);
+                boxes = cat(3, boxes, curBoxes);
+            end
+        end
     end
 end
 
-function flag = inBoxes(boxes, coordVec)
-    flag = any( ...
-        all(bsxfun(@ge, coordVec(:), boxes(:, 1, :)), 1) ...
-      & all(bsxfun(@lt, coordVec(:), boxes(:, 2, :)), 1));
+function boxes = barrelSplitFileBox(box, cubeIds, minSize, cubeSize)
+    relOff = cubeIds(:) .* cubeSize;
+    
+    % make relative to file
+    relBox = bsxfun(@minus, box, relOff);
+    relBox = [max(0, relBox(:, 1)), min(cubeSize, relBox(:, 2))];
+    maxBox = [zeros(3, 1), repmat(cubeSize, 3, 1)];
+    
+    boxes = barrelSplitCubeBox(relBox, maxBox, minSize);
+    boxes = bsxfun(@plus, boxes, relOff);
+end
+
+function boxes = barrelSplitCubeBox(box, maxBox, minSize)
+    
+    newBox = min(max(box, maxBox(:, 1)), maxBox(:, 2));
+    
+    % shortcut, if there is no overlap
+    if ~all(diff(newBox, 1, 2))
+        boxes = zeros(3, 2, 0);
+        return;
+    end
+    
+    box = newBox;
+    
+    % check if we can and have to split further
+    if any(box(:) ~= maxBox(:)) && any(diff(maxBox, 1, 2) > minSize)
+        newBox = [maxBox(:, 1), mean(maxBox, 2)];
+        newBoxMake = @(off) bsxfun( ...
+            @plus, newBox, diff(maxBox, 1, 2) ./ 2 .* off(:));
+        
+        boxes = cat(3, ...
+            barrelSplitCubeBox(box, newBoxMake([0, 0, 0]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([0, 0, 1]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([0, 1, 0]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([0, 1, 1]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([1, 0, 0]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([1, 0, 1]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([1, 1, 0]), minSize), ...
+            barrelSplitCubeBox(box, newBoxMake([1, 1, 1]), minSize));
+    else
+        boxes = maxBox;
+    end
 end
 
 function flag = isPowerOfTwo(vals)
