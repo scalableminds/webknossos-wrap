30b499f63aa4be21867bcd0296b6535f3d94a4b4
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-06-16 10:13:56 +0200

    Fix: Clean-up of command line utility

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,33 @@
 extern crate wkwrap as wkw;
 
 use std::env;
+use std::path::Path;
 
-fn recover_header() {
-    let pwd = env::current_dir().unwrap();
-    wkw::dataset::recover_header(pwd.as_path()).unwrap();
+fn recover_header(pwd: &Path) -> bool {
+    let recovery = wkw::dataset::recover_header(pwd);
+
+    if recovery.is_ok() {
+        verify_headers(pwd)
+    } else {
+        println!("Could not recover .wkw header");
+        false
+    }
 }
 
-fn verify_headers() {
-    let pwd = env::current_dir().unwrap();
-    let dataset = wkw::Dataset::new(pwd.as_path()).unwrap();
+fn verify_headers(pwd: &Path) -> bool {
+    let dataset = wkw::Dataset::new(pwd).unwrap();
     let okay = dataset.verify_headers().unwrap();
 
     if !okay {
         println!("Found .wkw file(s) with conflicting header");
     }
+
+    okay
 }
 
 fn main() {
     // parse input arguments
+    let pwd = env::current_dir().unwrap();
     let args: Vec<String> = env::args().collect();
     let arg_count = args.len();
 
@@ -28,9 +37,11 @@ fn main() {
     }
 
     // parse sub-command
-    match args[1].as_ref() {
-        "recover-header" => recover_header(),
-        "verify-headers" => verify_headers(),
-        _                => println!("Invalid sub-command")
-    }
+    let exit_code = match args[1].as_ref() {
+        "recover-header" => if recover_header(pwd.as_path()) { 0 } else { -2 },
+        "verify-headers" => if verify_headers(pwd.as_path()) { 0 } else { -3 },
+        _                => -1
+    };
+
+    std::process::exit(exit_code as i32);
 }
