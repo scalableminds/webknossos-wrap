4049f2e60fa86b29645461e368f4e5d05af8fdfb
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-06-21 22:33:17 +0200

    Del: LZ4 from list of exported modules

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,16 +1,20 @@
-pub use ::dataset::Dataset;
-pub use ::file::File;
-pub use ::header::Header;
-pub use ::mat::Mat;
-pub use ::morton::{Morton, Iter};
-pub use ::vec::{Box3, Vec3};
-pub use ::result::Result;
-
+// public modules
 pub mod dataset;
 pub mod file;
 pub mod header;
-pub mod lz4;
 pub mod mat;
 pub mod morton;
 pub mod result;
 pub mod vec;
+
+// private modules
+mod lz4;
+
+// convenience
+pub use dataset::Dataset;
+pub use file::File;
+pub use header::{Header, BlockType, VoxelType};
+pub use mat::Mat;
+pub use morton::{Morton, Iter};
+pub use result::Result;
+pub use vec::{Box3, Vec3};
