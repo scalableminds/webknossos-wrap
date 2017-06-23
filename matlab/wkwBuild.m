33307b5082cdc225973b8268b855e7873832cdbd
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-06-23 16:51:40 +0200

    Add: New build script for MATLAB

--- matlab/wkwBuild.m
+++ matlab/wkwBuild.m
@@ -2,24 +2,27 @@ function wkwBuild()
     % Written by
     %   Benedikt Staffler <benedikt.staffler@brain.mpg.de>
     %   Alessandro Motta <alessandro.motta@brain.mpg.de>
+    
+    buildWithCargo('wkw_load', 'wkwLoadRoi');
+    buildWithCargo('wkw_save', 'wkwSaveRoi');
+end
+
+function buildWithCargo(oldName, newName)
     prevDir = pwd();
+    
     thisDir = fileparts(mfilename('fullpath'));
-
-    % change to this directory
-    cd(thisDir);
-
-    mex -largeArrayDims CXXFLAGS='$CXXFLAGS' ...
-        LDOPTIMFLAGS='-O3' CXXOPTIMFLAGS='-O3' ...
-        -llz4 -I.. -I../libmorton/include wkwSave.cpp
-
-    mex -largeArrayDims CXXFLAGS='$CXXFLAGS' ...
-        LDOPTIMFLAGS='-O3' CXXOPTIMFLAGS='-O3' ...
-        -llz4 -I.. -I../libmorton/include wkwLoad.cpp
-
-    mex -largeArrayDims CXXFLAGS='$CXXFLAGS' ...
-        LDOPTIMFLAGS='-O3' CXXOPTIMFLAGS='-O3' ...
-        -llz4 -I.. -I../libmorton/include wkwCompress.cpp
-
-    % change back
+    cargoDir = fullfile(thisDir, 'rust', oldName);
+    
+    % build project
+    cd(cargoDir);
+    system('cargo build --release');
+    
+    % rename library
+    libDir = fullfile(cargoDir, 'target', 'release');
+    libPath = fullfile(libDir, strcat('lib', oldName, '.so'));
+    mexPath = fullfile(thisDir, strcat(newName, mexext()));
+    
+    movefile(libPath, mexPath);
+    
     cd(prevDir);
 end
