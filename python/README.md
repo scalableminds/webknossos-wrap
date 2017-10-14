e721d8d4f9f13ca5d21111551455f93f388b4c92
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-10-14 18:51:22 +0200

    Write a `Dataset` class for the Python library

--- python/README.md
+++ python/README.md
@@ -2,7 +2,7 @@
 
 ## To Do
 - [ ] Read in a C header file instead of hard-coding definitions
-- [ ] Write a `Dataset` class
+- [X] Write a `Dataset` class
 - [ ] Validate input values
 - [ ] Propagate errors from Rust
 
--- python/proto.py
+++ python/proto.py
@@ -1,3 +1,6 @@
+import os
+import ctypes
+import numpy as np
 from cffi import FFI
 
 ffi = FFI()
@@ -17,38 +20,54 @@ ffi.cdef("""
     void   dataset_get_header(const void * dataset, struct header * header);
 """)
 
-import os
 this_dir = os.path.dirname(__file__)
 path_libwkw = os.path.join(this_dir, '../c/target/debug/libwkw.so')
 C = ffi.dlopen(path_libwkw)
 
-root = ffi.new("char[]", b"/home/amotta/Desktop/wkw")
-dataset = C.dataset_open(root)
+class Dataset:
+    def __init__(self, root: str, handle):
+        self.root = root
+        self.handle = handle
 
-header = ffi.new("struct header *")
-C.dataset_get_header(dataset, header)
+        self.header = ffi.new("struct header *")
+        C.dataset_get_header(self.handle, self.header)
 
-print("Dataset: ", dataset)
-print("Header: ", header)
-print("  block_len: ", header.block_len)
-print("  file_len: ", header.file_len)
-print("  block_type: ", header.block_type)
-print("  voxel_type: ", header.voxel_type)
-print("  voxel_size: ", header.voxel_size)
+    def read(self, bbox):
+        data = np.zeros(bbox[:, 1] - bbox[:, 0], dtype='uint8', order='F')
+        bbox_ptr = ffi.cast("uint32_t *", bbox.ctypes.data)
+        data_ptr = ffi.cast("void *", data.ctypes.data)
+        C.dataset_read(self.handle, bbox_ptr, data_ptr)
 
-import numpy as np
-import ctypes
+        return data
 
-bbox = np.zeros((3, 2), dtype='uint32', order='F')
-bbox[:, 0] = [10, 20, 30]
-bbox[:, 1] = [70, 60, 50]
+    def close(self):
+        C.dataset_close(self.handle)
 
-data = np.zeros(bbox[:, 1] - bbox[:, 0], dtype='uint8', order='F')
+    @staticmethod
+    def open(root: str):
+        root_c = ffi.new("char[]", root.encode())
+        return Dataset(root, C.dataset_open(root_c))
 
-bbox_ptr = ffi.cast("uint32_t *", bbox.ctypes.data)
-data_ptr = ffi.cast("void *", data.ctypes.data)
-C.dataset_read(dataset, bbox_ptr, data_ptr)
+    def __enter__(self):
+        return self
 
-print("data: ", data)
+    def __exit__(self, type, value, tb):
+        self.close()
 
-C.dataset_close(dataset);
+root = "/home/amotta/Desktop/wkw"
+with Dataset.open(root) as dataset:
+    header = dataset.header
+    
+    print("Header:")
+    print("  block_len: ", header.block_len)
+    print("  file_len: ", header.file_len)
+    print("  block_type: ", header.block_type)
+    print("  voxel_type: ", header.voxel_type)
+    print("  voxel_size: ", header.voxel_size)
+    
+    bbox = np.zeros((3, 2), dtype='uint32', order='F')
+    bbox[:, 0] = [10, 20, 30]
+    bbox[:, 1] = [70, 60, 50]
+    
+    data = dataset.read(bbox)
+    print("data: ", data)
