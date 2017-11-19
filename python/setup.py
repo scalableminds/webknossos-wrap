80fa42e41be7f39fd462862241c146933d5aebe3
Alessandro Motta <alessandro.motta@brain.mpg.de>
2017-11-19 16:20:31 +0100

    Python: Fix indentation in setup file

--- python/setup.py
+++ python/setup.py
@@ -7,31 +7,31 @@ import shutil
 
 
 class BuildPyCommand(build_py):
-  """Modified build command to compile C library."""
+    """Modified build command to compile C library."""
 
-  def __build_c_library(self):
-      this_dir = os.path.dirname(__file__)
-      c_dir = os.path.normpath(os.path.join(this_dir, '..', 'c'))
+    def __build_c_library(self):
+        this_dir = os.path.dirname(__file__)
+        c_dir = os.path.normpath(os.path.join(this_dir, '..', 'c'))
 
-      # building C library
-      subprocess.call(['cargo', 'clean'], cwd=c_dir)
-      subprocess.call(['cargo', 'build', '--release'], cwd=c_dir)
+        # building C library
+        subprocess.call(['cargo', 'clean'], cwd=c_dir)
+        subprocess.call(['cargo', 'build', '--release'], cwd=c_dir)
 
-      lib_name = 'libwkw.so' # TODO(amotta): make this ready for Windows
-      lib_file = os.path.join(c_dir, 'target', 'release', lib_name)
+        lib_name = 'libwkw.so' # TODO(amotta): make this ready for Windows
+        lib_file = os.path.join(c_dir, 'target', 'release', lib_name)
 
-      # copying to lib dir
-      lib_dir = os.path.join(this_dir, 'wkw', 'lib')
+        # copying to lib dir
+        lib_dir = os.path.join(this_dir, 'wkw', 'lib')
 
-      if os.path.exists(lib_dir):
-          shutil.rmtree(lib_dir)
+        if os.path.exists(lib_dir):
+            shutil.rmtree(lib_dir)
 
-      os.makedirs(lib_dir)
-      shutil.copy(lib_file, os.path.join(lib_dir, lib_name))
+        os.makedirs(lib_dir)
+        shutil.copy(lib_file, os.path.join(lib_dir, lib_name))
 
-  def run(self):
-      self.__build_c_library()
-      build_py.run(self)
+    def run(self):
+        self.__build_c_library()
+        build_py.run(self)
 
 
 setup(
