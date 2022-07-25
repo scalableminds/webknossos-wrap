ee5e699bac8d64fa4ccffeb50f36c26aab8f8a21
Jonathan Striebel <jstriebel@users.noreply.github.com>
2022-07-25 11:21:12 +0200

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -87,6 +87,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
-    install_requires=["cffi", "numpy>=1.15,<1.22"],
+    install_requires=["cffi", "numpy>=1.15,==1.*"],
     cmdclass={"build_py": BuildPyCommand},
 )
