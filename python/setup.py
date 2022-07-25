d76185b2241833733b817915bacec02c210c1bf5
Jonathan Striebel <jstriebel@users.noreply.github.com>
2022-07-25 11:35:26 +0200

    formatting

--- python/setup.py
+++ python/setup.py
@@ -90,7 +90,7 @@ setup(
     install_requires=["cffi", "numpy>=1.15,==1.*"],
     extras_require={
         ':python_version < "3.8"': [
-            'numpy<1.22',
+            "numpy<1.22",
         ],
     },
     cmdclass={"build_py": BuildPyCommand},
